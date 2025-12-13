146fa8547a6fb298ec8c11aed6153a8e4319b7e7
chamodij <chamodijayasinghe23@gmail.com>
2025-12-13 17:13:02 +0530

    new

--- server/server.js
+++ server/server.js
@@ -11,5 +11,4 @@ app.get('/',(req,res) => res.send("API is working"))
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}
-    `);
-)
+    `));
